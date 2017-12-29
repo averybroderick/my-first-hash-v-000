a44159d9e57d5fa28d52c140e2443c9563efa86b
Avery Broderick <avery.c.broderick@gmail.com>
2017-12-29 17:27:53 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,7 +1,8 @@
 def my_hash
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
   a_hash = {
-    "name" => "avery"
+    "name" => "avery",
+    "age" => 25
   }
 
 end
